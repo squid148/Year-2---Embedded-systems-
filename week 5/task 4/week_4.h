4e239c8581cba4c1d13794c43c88162deda727d4
squid148 <tobyedawson@gmail.com>
2026-03-29 22:37:30 +0100

    Create week_4.h

---
+++ week 5/task 4/week_4.h
@@ -0,0 +1,39 @@
+//=====[Libraries]=============================================================
+//had to include libraries to ensure that header file knew what digital out and pins are
+#include "mbed.h"
+#include "arm_book_lib.h"
+
+
+//=====[Declaration and initialization of public global objects]=============
+extern DigitalOut sirenPin; //put external decleration in here to prevent undecleration or re-declerations from happening
+
+extern DigitalOut alarmLed; //had to put external dec leds to use in week 5 header 
+extern DigitalOut incorrectCodeLed;
+extern DigitalOut systemBlockedLed;
+
+extern UnbufferedSerial uartUsb;
+
+extern DigitalIn mq2;
+//=====[Declaration and initialization of public global variables]=============
+extern bool alarmState;
+extern Timer Reading_Timer;
+extern bool alarmState;
+extern bool gasDetectorState;
+extern bool overTempDetectorState;
+
+/*the header files for now only consist of prototyping of functions all other declerations and
+all other declerations, pin assignments etc are to go in to the cpp file*/
+//=====[Declarations (prototypes) of public functions]=========================
+void TempRead();
+void TempInC();
+void GasRead();
+void Potentiometer_Read();
+void SensorValPrint();
+void ThresholdSet();
+void AlarmCheck();
+void buzzer();
+void LM35ReadingsArrayInit();
+void OutputsInit();
+//=====[#include guards - end]=================================================
+
+#endif
